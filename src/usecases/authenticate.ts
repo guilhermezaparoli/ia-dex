15291102179848bfbd32936e03e31a6981995f3f
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-07-21 18:28:04 -0300

    feat: add authenticate use case

---
+++ src/usecases/authenticate.ts
@@ -0,0 +1,42 @@
+import { UsersRepository } from "@/repositories/users-repository.js";
+import { InvalidCredentialsError } from "./error/invalid-credentials-error.js";
+import * as argon2 from "argon2"
+import { User } from "@/generated/prisma/index.js";
+
+interface AuthenticateUseCaseRequest {
+    email: string
+    password: string
+}
+
+interface AuthenticateUseCaseResponse {
+    user: User
+    // token: string
+}
+
+export class AuthenticateUseCase {
+    constructor(private usersRepository: UsersRepository) { }
+
+
+    async execute({ email, password }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+
+
+        const user = await this.usersRepository.findByEmail(email)
+
+        if (!user) {
+            throw new InvalidCredentialsError()
+        }
+
+        const passwordIsValid = await argon2.verify(user.password_hash, password)
+
+        if (!passwordIsValid) {
+            throw new InvalidCredentialsError()
+        }
+
+
+        return {
+            user
+        }
+
+
+    }
+}
