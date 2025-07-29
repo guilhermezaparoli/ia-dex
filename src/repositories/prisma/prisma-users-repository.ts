8ebbc99dfa14e8f426d89454bfc363af0deb24c7
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-07-29 20:52:27 -0300

    feat: creating prisma users repository

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,24 @@
+import { prisma } from "@/lib/prisma/index.js";
+import { UsersRepository } from "../users-repository.js";
+import { Prisma, User } from "prisma/generated/prisma/index.js";
+
+export class PrismaUsersRepository implements UsersRepository {
+    async create(data: Prisma.UserCreateInput): Promise<User> {
+        const user = await prisma.user.create({
+            data
+        })
+
+        return user
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        return user
+    }
+}
