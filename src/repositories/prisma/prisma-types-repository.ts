583a233919dbe4846c17c043e446f80b0ce272bc
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-07-29 21:23:22 -0300

    feat: creating prisma types repository

---
+++ src/repositories/prisma/prisma-types-repository.ts
@@ -0,0 +1,26 @@
+import { prisma } from "@/lib/prisma/index.js";
+import { TypesRepository } from "../types-repository.js";
+import { Prisma, Type } from "prisma/generated/prisma/index.js";
+
+
+export class PrismaTypesRepository implements TypesRepository {
+    async findByName(name: string): Promise<Type | null> {
+        const type = await prisma.type.findUnique({
+            where: {
+                name
+            }
+        })
+
+        return type
+    }
+
+    async create(data: Prisma.TypeCreateInput): Promise<Type> {
+
+        const type = await prisma.type.create({
+            data
+        })
+
+        return type
+
+    }
+}
