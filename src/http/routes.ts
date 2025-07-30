d9b0a8587b041ad250c720deade9f4058ba28853
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-07-30 07:51:36 -0300

    feat: adding new rotes to create types

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,11 +1,22 @@
 import { FastifyInstance } from "fastify";
-import { monsters } from "./controller/monsters.js";
+
 import { register } from "./controller/register.js";
+import { authenticate } from "./controller/authenticate.js";
+import { verifyJWT } from "./middlewares/verify-jwt.js";
+import { createMonster } from "./controller/create-monsters.js";
+import { createType } from "./controller/create-type.js";
 
 
 export function AppRoutes(app: FastifyInstance) {
 
 
     app.post("/register", register)
-    app.post("/create-monster",monsters)
+    app.post("/auth", authenticate)
+
+    app.post("/monsters/create", {
+        onRequest: [verifyJWT]
+    }, createMonster)
+    app.post("/types/create", {
+        onRequest: [verifyJWT]
+    }, createType)
 }
