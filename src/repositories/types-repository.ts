e7b226bb495896ff779735aedc8505a1c1e37444
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-07-30 07:51:54 -0300

    feat: add types-repository

--- src/repositories/types-repository.ts
+++ src/repositories/types-repository.ts
@@ -1,6 +1,7 @@
-import { Prisma, Type } from "@/generated/prisma/index.js";
+import { Prisma, Type } from "prisma/generated/prisma/index.js"
 
 export interface TypesRepository {
     create(data: Prisma.TypeCreateInput): Promise<Type>
     findByName(name: string): Promise<Type | null>
+    findMany():Promise< Type[]>
 }
