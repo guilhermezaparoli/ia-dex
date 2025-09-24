2975c971e35fa7e487d9ff74afc8fff1ab015a07
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-09-24 07:44:40 -0300

    feat: creating inMemory and prisma repository to findUserByID

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -5,4 +5,5 @@ import { Prisma, User } from "prisma/generated/prisma/index.js"
 export interface UsersRepository {
     create(data: Prisma.UserCreateInput): Promise<User>
     findByEmail(email: string): Promise<User | null>
+    findById(id: string): Promise<User | null>
 }
