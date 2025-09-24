4514832bde11d8e904cbeeef98fec391054936f9
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-09-24 07:43:37 -0300

    feat: creating fetch user use case

---
+++ src/usecases/fetch-user.ts
@@ -0,0 +1,27 @@
+import { UsersRepository } from "@/repositories/users-repository.js";
+import { User } from "prisma/generated/prisma/index.js";
+import { ResourceNotFoundError } from "./error/resource-not-found-error.js";
+
+interface FetchUserUseCaseResponse {
+    user: Omit<User, "password_hash">
+}
+
+export class FetchUserUseCase {
+
+    constructor(private usersRepository: UsersRepository) { }
+
+
+    async exec(userId: string) {
+
+        const user = await this.usersRepository.findById(userId)
+
+        if (!user) {
+            throw new ResourceNotFoundError()
+        }
+
+
+        return {
+            user
+        }
+    }
+}
