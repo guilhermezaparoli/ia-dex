516b2b4fc8624feb2ff7f1e036441afc8ef05569
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-09-25 21:26:07 -0300

    feat: creating controller and route to get user data

--- src/http/controller/users/fetch-user.ts
+++ src/http/controller/users/fetch-user.ts
@@ -5,12 +5,19 @@ import { FastifyReply, FastifyRequest } from "fastify";
 export async function fetchUser(req: FastifyRequest, res: FastifyReply) {
 
 
+    const { sub } = req.user
 
     const { fetchUserUseCase } = makeFetchUser()
 
-    const { user } = await fetchUserUseCase.exec(req.user.sub)
+    const { user } = await fetchUserUseCase.exec(sub)
+
+    const safeUser = {
+      name: user.name,
+      email: user.email,
+      createdAt: user.created_at,
+    };
 
     res.status(200).send({
-        user
+        user: safeUser
     })
 }
--- src/http/controller/users/routes.ts
+++ src/http/controller/users/routes.ts
@@ -4,14 +4,17 @@ import { register } from "./register.js"
 import { refresh } from "./refresh.js"
 import { fetchUser } from "./fetch-user.js"
 import { logout } from "./log-out.js"
+import { verifyJWT } from "@/http/middlewares/verify-jwt.js"
 
-export function UsersRoutes(app: FastifyInstance){
+export function UsersRoutes(app: FastifyInstance) {
 
         app.post("/register", register)
         app.post("/auth", authenticate)
         app.post("/logout", logout)
 
-        app.get('/me', fetchUser)
-    
+        app.get('/me', {
+                onRequest: [verifyJWT]
+        }, fetchUser)
+
         app.patch("/token/refresh", refresh)
 }
