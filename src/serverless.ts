b387d96706b1ba8051401daea0e1c55530885860
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-10-14 20:24:53 -0300

    chore: remove Vercel deployment configuration and serverless handler

--- src/serverless.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "./app.js";
-
-// Export the app for Vercel serverless
-export default async (req: any, res: any) => {
-    await app.ready();
-    app.server.emit('request', req, res);
-};
